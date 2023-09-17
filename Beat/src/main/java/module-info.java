2a15cf958788fa3407b8a40737a3e5b16274d877
Je-RICO-h <erikpal566@gmail.com>
2023-09-17 12:57:38 +0200

    Created Main Project Maven Java files
    Created Using Java 19 And tested the sounds, the program has to be shutted down until we find a solution

---
+++ Beat/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.szoftmern.beat {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.szoftmern.beat to javafx.fxml;
+    exports com.szoftmern.beat;
+}
