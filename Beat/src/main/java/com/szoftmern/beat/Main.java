1ce3a6c90ed49ee3611a784aa9e139f0a41b3a89
Je-RICO-h <erikpal566@gmail.com>
2023-10-29 21:02:32 +0100

    Removed unnecesarry log files, clean up main
    Removed the unecesarry log files, and cleaned up the code in main

--- Beat/src/main/java/com/szoftmern/beat/Main.java
+++ Beat/src/main/java/com/szoftmern/beat/Main.java
@@ -3,12 +3,7 @@ package com.szoftmern.beat;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -45,8 +40,6 @@ public class Main extends Application {
         //Start Logging
         try {
             Logging.startLogging();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
