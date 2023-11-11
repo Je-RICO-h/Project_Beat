a858fe663a80525d3e2e66ac838b1d2b143e114d
czanita2 <cz.anita2002@gmail.com>
2023-11-12 00:12:29 +0100

    password visibility

--- Beat/src/main/java/com/szoftmern/beat/UIController.java
+++ Beat/src/main/java/com/szoftmern/beat/UIController.java
@@ -12,7 +12,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Control;
 import javafx.scene.control.*;
