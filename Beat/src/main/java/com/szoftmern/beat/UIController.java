b26ce24909858d9f21d871fe23b505da15d8459d
czanita2 <cz.anita2002@gmail.com>
2023-11-12 00:12:29 +0100

    password visibility

--- Beat/src/main/java/com/szoftmern/beat/UIController.java
+++ Beat/src/main/java/com/szoftmern/beat/UIController.java
@@ -162,7 +162,7 @@ public class UIController {
         }
     }
 
-    public static void makeNewStage(Event event, String file) {
+    public static void makeNewStage(Event event, String file) throws IOException {
         //new stage to make screen.fxml responsive
         FXMLLoader fxmlLoader = new FXMLLoader(UIController.class.getResource(file));
         Parent root1 = null;
