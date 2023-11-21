30c1b93fa7a63ccbce5a56b79a6537b2202f6744
alibowndyn <ppadm39@gmail.com>
2023-11-21 15:43:10 +0100

    Created new JPA Entity class in preparation for the country statistics

---
+++ Beat/src/main/java/com/szoftmern/beat/Country.java
@@ -0,0 +1,32 @@
+package com.szoftmern.beat;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "users")
+@EqualsAndHashCode
+@Entity
+@Table(name = "Countries", schema = "beat-db")
+public class Country {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private long id;
+
+    @Basic
+    @Column(name = "name")
+    private String name;
+
+    @Basic
+    @Column(name = "total_play_count")
+    private int total_play_count;
+
+    @OneToMany(mappedBy = "country")
+    private List<User> users;
+}
